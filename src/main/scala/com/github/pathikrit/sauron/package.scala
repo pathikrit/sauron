84030a6ed546c613f49988d8c586e44ccadae319
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-03-03 02:21:23 -0800

    non shadowing variable names

--- src/main/scala/com/github/pathikrit/sauron/package.scala
+++ src/main/scala/com/github/pathikrit/sauron/package.scala
@@ -4,39 +4,39 @@ import scala.reflect.macros.blackbox
 
 package object sauron {
 
-  def lens[A, B](obj: A)(path: A => B)(modifier: B => B): A = macro lensImpl[A, B]
+  def lens[A, B](obj: A)(pathSelector: A => B)(modifier: B => B): A = macro lensImpl[A, B]
 
   /**
    * lens(a)(_.b.c)(f) = a.copy(b = lens(a.b)(_.c)(f)
    * lens(x)(_)(f) = f(x)
    */
-  def lensImpl[A, B](c: blackbox.Context)(obj: c.Expr[A])(path: c.Expr[A => B])(modifier: c.Expr[B => B]): c.Tree = {
+  def lensImpl[A, B](c: blackbox.Context)(obj: c.Expr[A])(pathSelector: c.Expr[A => B])(modifier: c.Expr[B => B]): c.Tree = {
     import c.universe._
 
+    val (x, p, f) = (obj, pathSelector, modifier)
     def compileError(msg: String) = c.abort(c.enclosingPosition, msg)
 
     /**
-     * a.b.c -> List(a, b, c)
+     * _.p.q.r -> List(p, q, r)
      */
     def reverse(tree: c.Tree): List[c.TermName] = tree match {
-      case q"$ab.$c" => reverse(ab) :+ c
-      case t: Ident => Nil
+      case q"$pq.$r" => reverse(pq) :+ r
+      case _: Ident => Nil
       case _ => compileError(s"Unsupported path element: $tree")
     }
 
-    path.tree match {
-      case q"($arg) => $pathBody" => reverse(pathBody) match {
-        case b :: c :: d :: e :: f :: g :: h :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c.$d.$e.$f.$g.$h)($modifier))"
-        case b :: c :: d :: e :: f :: g :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c.$d.$e.$f.$g)($modifier))"
-        case b :: c :: d :: e :: f :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c.$d.$e.$f)($modifier))"
-        case b :: c :: d :: e :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c.$d.$e)($modifier))"
-        case b :: c :: d :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c.$d)($modifier))"
-        case b :: c :: Nil => q"$obj.copy($b = lens($obj.$b)(_.$c)($modifier))"
-        case b :: Nil => q"$obj.copy($b = lens($obj.$b)(x => x)($modifier))"
-        case Nil => q"$modifier($obj)"
-        case p => compileError(s"Too long of accessor path: ${p.length}")
+    p.tree match {
+      case q"($_) => $path" => reverse(path) match {
+        case p1 :: p2 :: p3 :: p4 :: p5 :: p6 :: Nil => q"$x.copy($p1 = lens($x.$p1)(_.$p2.$p3.$p4.$p5.$p6)($f))"
+        case p1 :: p2 :: p3 :: p4 :: p5 :: Nil => q"$x.copy($p1 = lens($x.$p1)(_.$p2.$p3.$p4.$p5)($f))"
+        case p1 :: p2 :: p3 :: p4 :: Nil => q"$x.copy($p1 = lens($x.$p1)(_.$p2.$p3.$p4)($f))"
+        case p1 :: p2 :: p3 :: Nil => q"$x.copy($p1 = lens($x.$p1)(_.$p2.$p3)($f))"
+        case p1 :: p2 :: Nil => q"$x.copy($p1 = lens($x.$p1)(_.$p2)($f))"
+        case p1 :: Nil => q"$x.copy($p1 = lens($x.$p1)(x => x)($f))"
+        case Nil => q"$f($x)"
+        case _ => compileError(s"Too long of accessor path: ${p.tree}")
       }
-      case _ => compileError(s"Path must have shape: _.a.b.c.(...), got: ${path.tree}")
+      case _ => compileError(s"Path must have shape: _.a.b.c.(...), got: ${p.tree}")
     }
   }
 }
