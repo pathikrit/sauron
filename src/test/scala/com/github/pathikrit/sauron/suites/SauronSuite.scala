350b4e9ef7866326a717bed5af569f0c34072b2a
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2015-03-02 21:10:04 -0800

    add unit tests

---
+++ src/test/scala/com/github/pathikrit/sauron/suites/SauronSuite.scala
@@ -0,0 +1,20 @@
+package com.github.pathikrit.sauron.suites
+
+import org.scalatest._, Matchers._
+
+class SauronSuite extends FunSuite {
+  import com.github.pathikrit.sauron._
+
+  test("basic lensing") {
+    case class Street(name: String)
+    case class Address(street: Street)
+    case class Person(address: Address)
+
+    val p1 = Person(Address(Street("s1")))
+    def changeName(n: String) = n+n
+
+    val p2 = modify(p1)(_.address.street.name).using(changeName)
+    p2 shouldEqual p1.copy(address = p1.address.copy(street = p1.address.street.copy(name = changeName(p1.address.street.name))))
+    "modify(p1)(_.address2.street.name).using(changeName)" shouldNot compile
+  }
+}
